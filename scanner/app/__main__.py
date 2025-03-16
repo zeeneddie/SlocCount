4d83ffba4f49acd553b085006b4e6ebb7b81619f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-16 06:08:34 +0000

    Analyse Repositories (#83)
    # Pull Request
    
    ## Description
    
    This pull request introduces the use of the `pygount` library to analyze
    source code in cloned repositories and updates the project dependencies
    accordingly. Below are the most important changes:
    
    ### Integration of `pygount` for Source Code Analysis:
    
    *
    [`scanner/app/__main__.py`](diffhunk://#diff-e370c8f7bf64354b82fe45c59db166c3b80722cbe29d17342bad06377b071639R4):
    Added imports for `ProjectSummary` and `SourceAnalysis` from `pygount`.
    Enhanced the `run_analyser` function to analyze files in cloned
    repositories using `pygount`, logging the analysis results and creating
    a project summary.
    [[1]](diffhunk://#diff-e370c8f7bf64354b82fe45c59db166c3b80722cbe29d17342bad06377b071639R4)
    [[2]](diffhunk://#diff-e370c8f7bf64354b82fe45c59db166c3b80722cbe29d17342bad06377b071639L35-R51)
    
    ### Dependency Updates:
    
    *
    [`scanner/pyproject.toml`](diffhunk://#diff-a3b82d49d76ebad61ba78436003c53e4907ae972476a9531807ceabd157d7586R9-R16):
    Added `pygount==2.0.0` to the list of dependencies. Updated the version
    of `ruff` in the optional development dependencies.
    [[1]](diffhunk://#diff-a3b82d49d76ebad61ba78436003c53e4907ae972476a9531807ceabd157d7586R9-R16)
    [[2]](diffhunk://#diff-a3b82d49d76ebad61ba78436003c53e4907ae972476a9531807ceabd157d7586R25-R27)
    
    Fixes #6

--- scanner/app/__main__.py
+++ scanner/app/__main__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from git import rmtree
+from pygount import ProjectSummary, SourceAnalysis
 from structlog import get_logger, stdlib
 
 from .configuration import Configuration
@@ -32,7 +33,22 @@ def run_analyser(configuration: Configuration) -> None:
     repositories = retrieve_repositories(configuration)
     for repository in repositories:
         owner_name, repository_name = repository.owner.login, repository.name
-        clone_repo(owner_name, repository_name)
+        folder_path = clone_repo(owner_name, repository_name)
+        project_summary = ProjectSummary()
+        iterator = Path(folder_path).walk()
+        for _root, _dirs, files in iterator:
+            for file in files:
+                file_path = f"{_root.__str__()}/{file}"
+                logger.debug("Analysing file", file=file_path)
+                file_analysis = SourceAnalysis.from_file(file_path, repository_name)
+                logger.debug("File analysis", file_analysis=file_analysis)
+                if file_analysis.language not in [
+                    "__unknown__",
+                    "__empty__",
+                    "__error__",
+                ]:
+                    project_summary.add(file_analysis)
+        logger.info("Project summary", project_summary=project_summary)
 
 
 def clean_up() -> None:
